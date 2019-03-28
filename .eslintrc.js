5d0274ab8459c8974d9620e9dbff9406708be464
Jun Wu <betastudio53@gmail.com>
2019-03-28 12:33:06 -0700

    config

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extend: 'hackreactor',
+    rules: {
+      // Rules here will override the 'hackreactor' configuration
+      // http://eslint.org/docs/rules/
+    }
+  };
+  
