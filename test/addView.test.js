bce36449142c4d1ea570e286891474da4e6ebcc1
Jun Wu <betastudio53@gmail.com>
2019-04-04 09:44:53 -0700

    components update

--- test/addView.test.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import News from '../client/src/components/EachNews.jsx';
--- test/dataCount.test.js
+++
@@ -1,7 +0,0 @@
-
-
-const data = require('../database/db.data1.js');
-
-test('counting how many company in database', () => {
-    expect(data.length).toBe(100);
-  });
